43e33314fc045c9cd91c10f17a7a552f15e6e14c
mmusgrave <mmusgrave@pobox.com>
2023-10-09 23:09:53 +0200

    Refactor functionality and add comments

--- crawler.py
+++ crawler.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 class Crawler:
     def __init__(self, url):
         self.url = url
+        self.relevant_post_data = []
 
 
     def __extract_relevant_post_data(self, posts, subtext): # List -> List -> List(Dict)
@@ -24,15 +25,21 @@ class Crawler:
 
 
     def crawl(self): # None -> List(Dict)
+        # Make GET request for page info and parse it with BeautifulSoup
         page = requests.get(self.url)
         soup = BeautifulSoup(page.content, "html.parser")
 
+        # Uses BeautifulSoup to get all relevant post information from 2 places
         forum_posts = soup.find_all("tr", {"class": "athing"}, limit=30)
         forum_subtext = soup.find_all("td", {"class": "subtext"}, limit=30)
 
-        return self.__extract_relevant_post_data(forum_posts, forum_subtext)
+        # Saves data in member variable for class instance for late use
+        self.relevant_post_data = self.__extract_relevant_post_data(forum_posts, forum_subtext)
+
+        # Returns relevant data
+        return self.relevant_post_data
 
 if __name__ == '__main__':
     crawler = Crawler("https://news.ycombinator.com/")
-    forum_data = crawler.crawl()
-    print(forum_data)
+    crawler.crawl()
+    print(crawler.relevant_post_data)
